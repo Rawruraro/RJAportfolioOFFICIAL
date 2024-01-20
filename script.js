9e7de054ba8d160848c4b7907bd56067f7d0b2b7
Rawruraro <151482753+Rawruraro@users.noreply.github.com>
2024-01-20 11:24:48 +0000

    some changes

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ if (navigator.getAutoplayPolicy("audio") === "allowed") {
   // will autoplay audio
 } else if (navigator.getAutoplayPolicy("audio") === "allowed-muted") {
   // Mute audio
-  video.muted = true;
+  audio.muted = true;}
