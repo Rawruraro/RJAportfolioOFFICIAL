be544f2270963ab8e933537fb9362def95f2fb9b
Rawruraro <151482753+Rawruraro@users.noreply.github.com>
2024-01-31 06:05:00 +0000

    almost done

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-if (navigator.getAutoplayPolicy("audio") === "allowed") {
-  // will autoplay audio
-} else if (navigator.getAutoplayPolicy("audio") === "allowed-muted") {
-  // Mute audio
-  audio.muted = true;}
+function validateLogin() {
+    var username = document.getElementById('username');
+    var password = document.getElementById('password');
+    var errorMessage = document.getElementById('errorMessage');
+
+    //Users
+    var validUsers = [
+        { username: 'Ratman', password: 'SavingRyansPrivates' },
+        { username: 'Rawru', password: 'RAWR1234' },
+        { username: 'Impartial', password: 'IMPARITALYOURB0Y' }
+    ];
+
+    var validUser = validUsers.find(user => username === username && password === password);
+
+    if (validUser) {
+        // on successful login
+        errorMessage.innerHTML = 'Login Success!';
+        alert('Login successful!');
+        window.location.href = 'Frontpage.html'; // Redirect to the home page
+    } 
+    else {
+        // on fail
+        errorMessage.innerHTML = 'Invalid username or password.';
+    }
+} 
+var username = document.getElementById('username');
+var content;
+
+  if (username === 'Ratman') {
+   content = 'Ratman is a young man who has been living in the shadows of his parents for the past'
+  } 
+  else if (username === 'Rawru'){
+    $('body').append('<h1>superman</h1>')
+
+  }
+  else if (username === 'Impartial'){
+    content = 'imparital is a young man who has been living in the shadows of his parents for the past'
+  }
