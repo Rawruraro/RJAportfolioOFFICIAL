9813ae26b6e04c0b29f8432285500ff650684eb0
Rawruraro <151482753+Rawruraro@users.noreply.github.com>
2024-02-07 06:46:08 +0000

    Added comments, Added signup page,  Changed videos and member page alignment

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function validateLogin() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value; 
-    var errorMessage = document.getElementById('errorMessage');
-    console.log("Username:", username);
-    localStorage.setItem('username', username);
+    var errorMessage = document.getElementById('errorMessage'); //will display text when username or password is wrong
+    console.log("Username:", username); //store username in console
+    localStorage.setItem('username', username); //store username in local storage, allowing it to be remembered as a preffered username
 
     // Users
-    var validUsers = [
+    var validUsers = [ //all valid usernames and passwords
         { username: 'Ratman', password: 'SavingRyansPrivates' },
         { username: 'Rawru', password: 'RAWR1234' },
         { username: 'Impartial', password: 'IMPARITALYOURB0Y' }
     ];
 
-    var validUser = validUsers.find(user => user.username === username && user.password === password); 
+    var validUser = validUsers.find(user => user.username === username && user.password === password); //checks if username and password are valid
 
     if (validUser) {
         // On successful login
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var el = document.getElementById('content');
     var content;
 
-    if (storedUsername === 'Ratman') {
+    if (storedUsername === 'Ratman') { //changes the link in "content" depending on the username
         content = '<a href="https://29508913-8096-4ddd-813b-d6b9165a0165-00-7i2vgpaoho8b.picard.repl.co/">Andrei Resume</a>';
     } else if (storedUsername === 'Rawru') {
         content = '<a href="https://035e79d6-fc53-4ba1-9dee-9bdf6200d4dd-00-imb2m36ld53q.riker.replit.dev/">Rolando Resume</a>';
@@ -38,5 +38,37 @@ document.addEventListener('DOMContentLoaded', function () {
         content = '<a href="https://a4b22d59-9a88-44d7-9f02-0e8e28893961-00-192d75gwpyto0.janeway.replit.dev/">Josh Resume</a>';
     }
 
-    el.insertAdjacentHTML('afterbegin', content);
+    el.insertAdjacentHTML('afterbegin', content); //places the new link in "content"
 });
+
+function signUp() {
+  var name = document.getElementById('name').value;
+  var pass = document.getElementById('pass').value;
+  var confirmPass = document.getElementById('confirmPass').value;
+  var Yrlvl = document.getElementById('Yrlvl').value;
+  var Favclr = document.getElementById('Favclr').value;
+  var Favfood = document.getElementById('Favfood').value;
+  var signupMessage = document.getElementById('signupMessage');
+
+  if (confirmPass == pass){
+    signupMessage.innerHTML = 'Sign Up Success!';
+  
+    var signedName = document.getElementById('signedName');
+    signedName.innerHTML = 'Name: ' + name;
+  
+    var signedPass = document.getElementById('signedPass');
+    signedPass.innerHTML = 'Password: ' + pass;
+  
+    var signedYrlvl = document.getElementById('signedYrlvl');
+    signedYrlvl.innerHTML = 'Year Level: ' + Yrlvl;
+  
+    var signedFavclr = document.getElementById('signedFavclr');
+    signedFavclr.innerHTML = 'Favorite Color: ' + Favclr;
+  
+    var signedFavfood = document.getElementById('signedFavfood');
+    signedFavfood.innerHTML = 'Favorite Food: ' + Favfood;
+  }
+  else {
+    signupMessage.innerHTML = 'Password does not match!';
+  }
+}
